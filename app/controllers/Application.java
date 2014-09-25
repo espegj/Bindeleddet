264c9d3c8ba057799cd3398da70bde3de75c63c0
espen <espen.gjernes@kebas.no>
2014-09-25 12:22:02 +0200

    Navbar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,38 +1,30 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Query;
-
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.SqlRow;
-import com.avaje.ebean.SqlUpdate;
-
-import play.*;
 import play.db.ebean.Transactional;
+
 import play.mvc.*;
-import scala.Int;
 import views.html.*;
 
 public class Application extends Controller {
 
 	@Transactional
     public static Result index() { 
-	
-//        SqlUpdate insert = Ebean.createSqlUpdate("INSERT INTO db values(2)");
-//        insert.execute(); 
-//        
-//        String sql = "select count(*) as count from db";  
-//        SqlRow row =   
-//            Ebean.createSqlQuery(sql)  
-//            .findUnique();  
-//          
-//        Integer i = row.getInteger("count");  
-//          
-//        System.out.println(i);  
-       
-		System.out.println("test");
+		
+//		RawSql rawSql = RawSqlBuilder.parse("SELECT * from linje").create();
+
+		
+		
+		String sql = "select count(*) as count from linje";  
+        SqlRow row =   
+            Ebean.createSqlQuery(sql)  
+            .findUnique();  
+          
+        Integer i = row.getInteger("count");  
+          
+        System.out.println(i); 
+			
  
 		
 		
