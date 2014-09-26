651887e9fc9b0e7af911895b7a77f34506b9a8a2
espen <espen.gjernes@kebas.no>
2014-09-26 17:43:38 +0200

    databasekall

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,17 +13,20 @@ import com.avaje.ebean.RawSqlBuilder;
 import com.avaje.ebean.SqlRow;
 import com.avaje.ebean.SqlUpdate;
 
+import play.Logger;
+import play.data.DynamicForm;
 import play.data.Form;
 import play.db.ebean.Transactional;
 import play.mvc.*;
+import play.mvc.Http.MultipartFormData;
 import models.linje;
 import views.html.*;
 
 public class Application extends Controller {
 
-	@Transactional
+	
 	public static Result index() {
-		return ok(index.render("Your new application is ready."));
+		return ok(index.render("Bindeleddet"));
 	}
 
 	public static Result login() {
@@ -31,11 +34,52 @@ public class Application extends Controller {
 	}
 	
 	public static Result addAdvertisement() {
-		return ok(addAdvertisement.render("Log inn"));
+		return ok(addAdvertisement.render("Legg til"));
 	}
 	
+	@Transactional
+	public static Result uploadData() {
+		
+		ArrayList<String> input = new ArrayList<>();
+		ArrayList<String> inputValue = new ArrayList<>();
+		
+		DynamicForm dynamicForm = form().bindFromRequest();
+		input.add("info");
+		input.add("typeId");
+		input.add("varighet");
+		input.add("linjeId");
+		input.add("trinnId");
+		input.add("url");
+		input.add("kontaktNavn");
+		input.add("kontaktEmail");
+		input.add("frist");
+		input.add("prioritet");
+		input.add("sted");
+		input.add("bedriftsNavn");
+		input.add("tittel");
+		
+		for(String i : input){
+			if(dynamicForm.get(i) == null || dynamicForm.get(i) == ""){
+				if(i=="prioritet"){
+				inputValue.add("0");
+				}
+				inputValue.add("");
+			}
+		
+			else{
+				inputValue.add(dynamicForm.get(i));	
+			}
+		}
 
-	
-	
-
+		Databasen db = new Databasen();
+		db.insert("annonse", inputValue.get(0), Integer.parseInt(inputValue.get(1)), Integer.parseInt(inputValue.get(2)),
+				Integer.parseInt(inputValue.get(3)), Integer.parseInt(inputValue.get(4)), inputValue.get(5), inputValue.get(6), 
+				inputValue.get(7), inputValue.get(8), Integer.parseInt(inputValue.get(9)), inputValue.get(10), inputValue.get(11), 
+				inputValue.get(12));
+		
+		
+		return ok(index.render("Bindeleddet"));
+		
+		    
+	}
 }
--- app/controllers/Databasen.java
+++ app/controllers/Databasen.java
@@ -3,26 +3,33 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import play.db.ebean.Transactional;
+
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.SqlRow;
 import com.avaje.ebean.SqlUpdate;
 
+
+@Transactional
 public class Databasen {
-	
-	
-	
-	public void insert(String table, String name, String value){
-		SqlUpdate insert = Ebean.createSqlUpdate("insert into "+table+" ("+name+") values(\""+value+"\")");
-		insert.execute(); 
+
+	public void insert(String table, String info, Integer typeId, Integer varighet,
+			Integer linjeId, Integer trinnId, String url, String kontaktNavn, String kontaktEmail,
+			String frist, Integer prioritet, String sted, String bedriftsNavn, String tittel) {
+		SqlUpdate insert = Ebean.createSqlUpdate("insert into " + table + " values(null,\"" + info + "\",\"" + typeId + "\", \"" + varighet + "\",\"" + linjeId
+				+ "\",\"" + trinnId + "\",\"" + url + "\",\"" + kontaktNavn + "\",\""
+				+ kontaktEmail + "\",\"" + frist + "\",\"0\",\"" + Integer.toBinaryString(prioritet) + "\",\"" + sted
+				+ "\",\"" + bedriftsNavn + "\",\"" + tittel + "\")");
+		insert.execute();
 	}
-	
-	public ArrayList<String> select(String query){
+
+	public ArrayList<String> select(String query) {
 		ArrayList<String> list = new ArrayList<>();
 		String sql = query;
-		List<SqlRow> sqlRows = Ebean.createSqlQuery(sql).findList();		 
+		List<SqlRow> sqlRows = Ebean.createSqlQuery(sql).findList();
 		for (SqlRow sqlRow : sqlRows) {
-		    String name = sqlRow.getString("navn");
-		    list.add(name);
+			String name = sqlRow.getString("navn");
+			list.add(name);
 		}
 		return list;
 	}
