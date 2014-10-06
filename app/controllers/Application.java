1b7a131168678b4ef8203d09913de29a7747b3dd
Thomas <thomas.af92@gmail.com>
2014-10-06 13:28:28 +0200

    angular

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,23 +1,20 @@
 package controllers;
 
-import static play.data.Form.form;
-
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
-import akka.actor.dsl.Inbox.Select;
+import play.db.ebean.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
 
 import com.avaje.ebean.Ebean;
-import com.avaje.ebean.RawSql;
-import com.avaje.ebean.RawSqlBuilder;
 import com.avaje.ebean.SqlRow;
-import com.avaje.ebean.SqlUpdate;
-
-import play.data.Form;
-import play.db.ebean.Transactional;
-import play.mvc.*;
-import models.linje;
-import views.html.*;
 
 public class Application extends Controller {
 
@@ -25,8 +22,36 @@ public class Application extends Controller {
 	public static Result index() {
 		return ok(index.render("Your new application is ready."));
 	}
+	
+	public static Result json() {
+		return ok(json.render("Your new application is ready."));
+	}
+
+	
+	public static Result detail() {
+		
+		return ok(detail.render(""));
+	}
 
 	public static Result login() {
+		try {
+			 
+			String content = "This is the content to write into file";
+ 
+			File file = new File("/home/thomas/Dokumenter/Bindeleddet/public/jSon/filename.txt");
+ 
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+			
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return ok(login.render("Log inn"));
 	}
 	
@@ -34,7 +59,34 @@ public class Application extends Controller {
 		return ok(addAdvertisement.render("Log inn"));
 	}
 	
+	@Transactional
+	public static Result angular() throws IOException {
+		String sql = "Select * from annonse"; 
+		List<SqlRow> sqlRows = Ebean.createSqlQuery(sql).findList();
+		String test = "TEST[{\"annonseid\":7, \"info\":\"info\", \"typeid\":1, \"varighet\":2, \"linjeid\":1, \"trinnid\":1, \"url\":\"eyfe\", \"kontaktnavn\":\"Petter\", \"kontaktemail\":\"mail\", \"frist\":\"2014-09-10\", \"teller\":0, \"prioritet\":\"true\", \"sted\":\"grimstad\", \"bedriftsnavn\":\"Feier as\", \"tittel\":\"Feier\"}]";
+		
+		String fileName = "/home/thomas/Dokumenter/Bindeleddet/public/jSon/phones.json";
+		
+		    BufferedReader br = new BufferedReader(new FileReader(fileName));
+		    try {
+		        StringBuilder sb = new StringBuilder();
+		        String line = br.readLine();
 
+		        while (line != null) {
+		            sb.append(line);
+		            sb.append("\n");
+		            line = br.readLine();
+		        }
+		        test = sb.toString();
+		        StringBuilder contentInnerHtml = new StringBuilder();
+		        contentInnerHtml.append(test);
+		    } finally {
+		        br.close();
+		    }
+		    return ok(angular.render(test));
+		}
+		
+	
 	
 	
 
